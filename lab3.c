dabe0155735ad01aef0475a5164a96e5e2435951
6704062611352 <s6704062611352@email.kmutnb.ac.th>
2025-12-16 13:57:13 +0700

    Add files via upload

---
+++ lab3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int score;
+    scanf("%d",&score);
+    if(score>=68){
+        if(score>=87){
+            printf("A");
+        }else if(score>=79){
+            printf("B");
+        }else if(score>=71){
+            printf("C");
+        }
+       
+    }else{
+        if(score<=63){
+            if(score>12){
+                printf("D");
+            }else{
+                printf("F");
+            }
+        }
+    }
+
+    return 0;
+}
