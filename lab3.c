361b3df1427b2e0142d2d6c1add057592f15d7e5
6704062611352 <s6704062611352@email.kmutnb.ac.th>
2025-12-16 14:03:39 +0700

    Bonus2

--- lab3.c
+++ lab3.c
@@ -4,25 +4,30 @@ int main()
 {
     int score;
     scanf("%d",&score);
-    if(score>=68){
-        if(score>=87){
-            printf("A");
-        }else if(score>=79){
-            printf("B");
-        }else if(score>=71){
-            printf("C\n");
-            printf("B is %d",79-score);
-        }
-       
-    }else{
-        if(score<=63){
-            if(score>12){
-                printf("D");
-            }else{
-                printf("F");
+    if(score<=100){
+        if(score>=68){
+            if(score>=87){
+                printf("A");
+            }else if(score>=79){
+                printf("B");
+            }else if(score>=71){
+                printf("C\n");
+                printf("B is %d",79-score);
+            }
+           
+        }else{
+            if(score<=63){
+                if(score>12){
+                    printf("D");
+                }else if(score<0){
+                    printf("error score");
+                }else{
+                    printf("F");
+                }
             }
         }
+    }else{
+        printf("error score");
     }
-
     return 0;
 }
